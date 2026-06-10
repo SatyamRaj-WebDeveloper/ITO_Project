404c2a782f657ee6a1e1029ba9edc6c187ac3bbf
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2026-06-10 11:48:01 +0530

    DB configurations changed a bit

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,8 +8,12 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  idleTimeoutMillis: 30000, // Automatically drops idle connections to protect server memory
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 15000,
+  idleTimeoutMillis: 30000, 
+  max : 20,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.on('connect', () => {
